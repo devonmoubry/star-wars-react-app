4776dc7decb12b621234ae80cfaf10a0a03f3dc8
Devon Moubry <devon@moubry.com>
2017-09-12 17:08:16 -0500

    initial commit

---
+++ src/components/Jumbotron.js
@@ -0,0 +1,11 @@
+import React, {Component} from 'react';
+export default class Jumbotron extends Component{
+  render() {
+    return (
+      <div>
+        <h1>Star Wars</h1>
+        <p>The Vehicles of Star Wars</p>
+      </div>
+    )
+  }
+}
